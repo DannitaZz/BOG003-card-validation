bc5d844f28690839f9702381f10bf569b44edb90
Danna <thedannitazz@gmail.com>
2021-06-21 18:55:37 -0500

    Se le agregó el input del número de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+memoria = {};
+
+function myFunction() {
+
+    var passField = document.getElementById('pass');
+    passField.type = "text";
+    let input_tarjeta = document.getElementById('pass');
+      memoria['tarjeta'] = input_tarjeta.value;
+      console.log(memoria['tarjeta'])
+    var passValue = passField.value;
+    var passLength = passValue.length;
+    var masked = passValue.substring(0, passLength - 4);
+    masked = masked.replace(/./g, '•'); //The character is ASCII-7 (Press Alt+7 to type)
+    var text = passValue.substring(passLength - 4);
+    var newPass = masked + text;
+    passField.value = newPass;
+    
+    return newPass
+  }
+  
+  
