548030eeba87ed5e478d59372e6523179f07ee66
Danna <thedannitazz@gmail.com>
2021-06-21 19:57:43 -0500

    Pasando maskify a validator

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,31 @@
 const validator = {
-  // ...
+  memoria = {};
+
+  maskify: function () {
+
+    var passField = document.getElementById('creditCardNumber');
+    passField.type = "text";
+    let input_tarjeta = document.getElementById('creditCardNumber');
+      memoria['creditCardNumber'] = input_tarjeta.value;
+      console.log(memoria['creditCardNumber'])
+    var passValue = passField.value;
+    var passLength = passValue.length;
+    var masked = passValue.substring(0, passLength - 4);
+    masked = masked.replace(/./g, '#'); //The character is ASCII-7 (Press Alt+7 to type)
+    var text = passValue.substring(passLength - 4);
+    var newPass = masked + text;
+    passField.value = newPass;
+    
+    return newPass
+  }
+
+
+  
+
+
+  
+
+  
 };
 
 export default validator;
