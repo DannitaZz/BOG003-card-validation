ca59a2ef81f3747b2413cfb4cda1e4c26bb1b1f9
Danna <thedannitazz@gmail.com>
2021-06-22 08:35:33 -0500

    Puse cada código en el archivo correspondiente, pero aún no funciona

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import validator from './validator.js';
+import  validator   from "./validator.js";
 
-memoria = {};
-console.log(validator);
+//* const saludo = validator.hola();
+
+
+
+const masky = validator.maskify();
+
+function extraeNumero() {
+  var passField = document.getElementById('pass');
+    passField.type = "text";
+  var input_tarjeta = document.getElementById('pass');
+  var creditCardNumber = input_tarjeta.value;
+      
+
+    return creditCardNumber
+}  
+
+const extrae = extraeNumero();
+
+function cubreNumero() {
+
+  passField.value = newPass;
+
+}  
+
+const cubre = cubreNumero();
+
+
+
+
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,23 +1,20 @@
 const validator = {
-  memoria = {};
+  
+  
 
-  maskify: function () {
+  maskify: function (creditCardNumber) {
 
-    var passField = document.getElementById('creditCardNumber');
-    passField.type = "text";
-    let input_tarjeta = document.getElementById('creditCardNumber');
-      memoria['creditCardNumber'] = input_tarjeta.value;
-      console.log(memoria['creditCardNumber'])
-    var passValue = passField.value;
-    var passLength = passValue.length;
-    var masked = passValue.substring(0, passLength - 4);
+    var cardLength = creditCardNumber.length;
+    var masked = creditCardNumber.substring(0, cardLength - 4);
     masked = masked.replace(/./g, '#'); //The character is ASCII-7 (Press Alt+7 to type)
-    var text = passValue.substring(passLength - 4);
+    var text = creditCardNumber.substring(cardLength - 4);
     var newPass = masked + text;
-    passField.value = newPass;
-    
     return newPass
-  }
+  },
+
+  // hola: function () {
+    //console.log("Hola mundo");
+  //} 
 
 
   
