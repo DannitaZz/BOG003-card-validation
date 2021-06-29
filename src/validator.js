de251f38fea6600ffdbdadc89c784d173c3f623b
Danna <thedannitazz@gmail.com>
2021-06-29 11:25:46 -0500

    isValid modificación

--- src/validator.js
+++ src/validator.js
@@ -16,24 +16,31 @@ const validator = {
   isValid : (creditCardNumber) => {
   
     let cardNumber = 0, estado = false;
-    
-  
     for (let n = creditCardNumber.length - 1; n >= 0; n--) {
       let cDigito = creditCardNumber.charAt(n);
       let	nDigito = parseInt(cDigito, 10);
   
      
-     if (estado && (nDigito *= 2) > 9) {
-      nDigito -= 9;
-     }
+     if (estado) {
+       nDigito *= 2;
+       //console.log(nDigito)
+      
+        if ((nDigito*2) > 9){
+          nDigito-=9;
+        }
+      }
+     
   
       cardNumber += nDigito;
       estado = !estado;
+      //console.log(cardNumber)
     }
     
     
     return (cardNumber % 10) == 0;
-  }  
+   
+  }
+  
 
 
 
